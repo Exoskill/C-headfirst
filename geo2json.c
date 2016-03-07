21132ae70cb03c824d93a2768dc2072605f8d744
raphael leray <r.leray@lecreuset.co.jp>
2016-03-07 16:29:38 +0900

    error output

--- geo2json.c
+++ geo2json.c
@@ -14,6 +14,17 @@ int main()
             printf(",\n");
         else
             started = 1;
+        
+        if((latitude < -90.0) || (latitude > 90.0)){
+            printf("Invalid latitude: %f\n", latitude);
+            return 2;
+        }
+
+        if((longitude < -180.0) || (longitude > 180.0)){
+            printf("Invalid longitude %f\n", longitude);
+            return 2;
+        }
+
         printf("{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
     }
     puts("\n]");
